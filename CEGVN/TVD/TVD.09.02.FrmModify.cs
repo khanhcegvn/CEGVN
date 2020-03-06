9755cd207502a28db7fe69fc829657d9c6cf4cff
do toan <toan1106@gmail.com>
2020-03-06 08:51:19 +0700

    ssss

---
+++ CEGVN/TVD/TVD.09.02.FrmModify.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Windows.Forms;
+using a = Autodesk.Revit.DB;
+using c = Autodesk.Revit.ApplicationServices;
+
+
+namespace CEGVN.TVD
+{
+    public partial class FrmModify : Form
+    {
+        private AddParameterFamily _data;
+        private a.Document _doc;
+        private c.Application _app;
+        public FrmModify(AddParameterFamily data, a.Document doc, c.Application app)
+        {
+            _data = data;
+            _doc = doc;
+            _app = app;
+            InitializeComponent();
+        }
+
+        private void FrmModify_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_OK_Click(object sender, EventArgs e)
+        {
+            lbr ORI = new lbr();
+            if (EMBEDSTANDARD.Checked)
+            {
+                ORI.RemoveShareParameter(_doc);
+                ORI.EmbedStandard(_doc, _app);
+            }
+            if (EMBEDSTANDARD_finish.Checked)
+            {
+                ORI.RemoveShareParameter(_doc);
+                ORI.EmbedStandard_finish(_doc, _app);
+            }
+            if (EMBEDCUSTOM.Checked)
+            {
+                ORI.RemoveShareParameter(_doc);
+                ORI.EmbedCustom(_doc, _app);
+            }
+            if (EMBEDCUSTOM_FINISH.Checked)
+            {
+                ORI.RemoveShareParameter(_doc);
+                ORI.EmbedCustom_finish(_doc, _app);
+            }
+            if (CIPSTANDARD.Checked)
+            {
+                ORI.RemoveShareParameter(_doc);
+                ORI.CIPSTANDARD(_doc, _app);
+            }
+            if (CIPCUSTOM.Checked)
+            {
+                ORI.RemoveShareParameter(_doc);
+                ORI.CIPCUSTOM(_doc, _app);
+            }
+            if (ERECTIONSTANDARD.Checked)
+            {
+                ORI.RemoveShareParameter(_doc);
+                ORI.ERECTIONSTANDARD(_doc, _app);
+            }
+            if (ERECTIONSTANDARD_FINISH.Checked)
+            {
+                ORI.RemoveShareParameter(_doc);
+                ORI.ERCTIONSTANDARD_finish(_doc, _app);
+            }
+            if (Keep_data.Checked)
+            {
+                ORI.RemoveShareParameterkeep(_doc);
+            }
+            Close();
+
+        }
+
+        private void EMBED_CUSTTOM_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Cancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void radioButton1_CheckedChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
